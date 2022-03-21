2f92693e2d0ccdfcd0ffc47791a97e945e8efc68
Hasegawa-Akito <h.akito1999@gmail.com>
2022-03-21 18:27:58 +0900

    create header

---
+++ src/component/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    
+    <header class="p-3 bg-dark text-white d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none text-whit">
+            <span class="fs-4">ポートフォリオ一覧</span>
+        </div>
+        
+        {/*
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+        */}
+    </header>
+    
+  )
+}
+
+export default Header
