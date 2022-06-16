29a1d2e0bc18bd4a09b80c597d24139af0cab068
Hasegawa-Akito <h.akito1999@gmail.com>
2022-06-16 16:53:11 +0900

    ポートフォリオの一部を削除

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -9,15 +9,7 @@ const Header = () => {
             <span class="fs-4">Portfolio List</span>
         </div>
         
-        {/*
-        <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-        </ul>
-        */}
+        
     </header>
     
   )
